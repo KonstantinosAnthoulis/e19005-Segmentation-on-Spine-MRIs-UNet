d3eace353736b0d18a8a579b3cdea0de58606f07
KonstantinosAnthoulis <151029554+KonstantinosAnthoulis@users.noreply.github.com>
2024-01-26 14:48:09 +0200

    get center of array to create new files to return for nonzero
    function gets 2 3d arrays as input
    will create 2 new 3d arrays of dimensions
    [ array.shape[0], non zero x lines rounded up to multiple of 16, non zero y lines rounded up to multiple of 16]
    
    then will go through each slice in the original 3d array and get the center crop and add it to the new array
    
    return new arrays

--- transforms/array_transforms.py
+++ transforms/array_transforms.py
@@ -54,25 +54,33 @@ def crop_zero(img_a, label_a):
     x_max_list = []
     y_max_list = []
 
+     #finding start and end of non-zero cols rows
+    row_start_idx = 0 #reset for each mri
+    row_end_idx = 0 #reset for each mri
+    col_start_idx = 0 #reset for each mri
+    col_end_idx = 0 #reset for each mri
+
     for idx in range(label_a.shape[0]): #go through non empty slices
         row_count = 0 #reset for each slice
         col_count = 0 #reset for each slice 
 
-        #finding start and end of non-zero cols rows
-        row_start = 0 #reset for each slice
-        row_end = 0 #reset for each slice
-        col_start = 0 #reset for each slice
-        col_end = 0 #reset for each slice
-        
-        #counting 0 lines, using this util for now but there is a chance it could find and delete cols/rows of 0 within the region of interest
-        
+        #counting 0 rows, using this util for now but there is a chance it could find and delete cols/rows of 0 within the region of interest
         for idx_row in range(label_a.shape[1]): #for rows
             if  np.any(label_a[idx, idx_row, :]):
                 row_count = row_count + 1
+                if(row_start_idx == 0): #check if first non zero row 
+                    row_start_idx = idx_row
+                if np.any(label_a[idx, idx_row + 1, :]): #check if last non zero row
+                    row_end_idx = idx_row 
 
         for idx_col in range(label_a.shape[2]): #for columns
             if  np.any(label_a[idx, : ,idx_col]):
                 col_count = col_count +1
+                if(col_start_idx == 0): #check if first non zero col 
+                    col_start_idx = idx_col
+                if np.any(label_a[idx, :, idx_col + 10]): #check if last non zero col
+                    col_end_idx = idx_col
+
 
         #max_list.append([idx, row_count, col_count]) #index of slice in mri, row count, col count  
         x_max_list.append(row_count)
@@ -90,12 +98,24 @@ def crop_zero(img_a, label_a):
     print("x max", x_max_nonzero)
     print("y max", y_max_nonzero)
 
+    print("row start idx", row_start_idx)
+    print("row end idx", row_end_idx)
+    print("col start idx", col_start_idx )
+    print("col end idx", col_end_idx)
+
+
+
     #TODO create the new arrays to return all the non_zero rows cols 
-     
 
-    out_img_a = np.empty([img_a.shape[0],x_max_nonzero, y_max_nonzero])
-    out_label_a = np.empty([label_a.shape[0], x_max_nonzero, y_max_nonzero])
+    #out_img_a = np.empty([img_a.shape[0],x_max_nonzero, y_max_nonzero])
+    #out_label_a = np.empty([label_a.shape[0], x_max_nonzero, y_max_nonzero])
 
+    print("/2 row", img_a.shape[1]/2)
+    print("/2 col", img_a.shape[2]/2 )
+
+    out_img_a = img_a[ img_a.shape[1]//2 - x_max_nonzero//2:img_a.shape[1] + x_max_nonzero//2 , img_a.shape[2]//2 - y_max_nonzero//2:img_a.shape[2] + y_max_nonzero//2 , img_a.shape[0]]
+
+    print(out_img_a.shape)
     return x_max_nonzero, y_max_nonzero #temporary just to get max dims
 
     #TODO find the rows and cols where nonzeros start and end and from that index start adding to nonzero array
