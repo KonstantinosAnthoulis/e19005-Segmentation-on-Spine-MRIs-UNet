a6c95c55da5e6a267ff9b552efc11f74c09ef656
Konstantinos Anthoulis <151029554+KonstantinosAnthoulis@users.noreply.github.com>
2024-10-07 16:50:07 +0300

    augmentation script TODO execute augmentaiotn using albumentations

---
+++ preprocessing/augmentation.py
@@ -0,0 +1,11 @@
+#PREPROCESSING SOMETHING
+
+#Data Augmentation
+
+#Generate X amount of slices from 1 slice to further increase the dataset size 
+
+#Dependencies 
+import albumentations as a
+
+
+print("ok")
