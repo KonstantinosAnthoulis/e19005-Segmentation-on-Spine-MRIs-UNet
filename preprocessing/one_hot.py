78369ca8602ee3680f9675585c9118ce2f1bada6
KonstantinosAnthoulis <151029554+KonstantinosAnthoulis@users.noreply.github.com>
2024-10-30 20:47:24 +0200

    get doesn't exist for cupy

--- preprocessing/one_hot.py
+++ preprocessing/one_hot.py
@@ -17,13 +17,18 @@ def one_hot_encoding(masks_no, masks_array):
 
 def one_hot_encoding_cp(masks_no, masks_array): 
     val_range = cp.arange(masks_no)  # Use CuPy's arange
-    mapping_dict = {k: v for k, v in zip(masks_array, val_range)}  # Mapping dictionary remains the same
-    
+    mapping_dict = {k: v for k, v in zip(masks_array, val_range)}  # Create a dictionary with mappings
+
     # Define a function to perform the mapping using the dictionary
     def map_to_specified_set(value):
-        return mapping_dict.get(value, cp.nan)  # Return cp.nan for values not found in the mapping_dict
-    
+        # Use `in` to check if the value exists in the dictionary
+        if value in mapping_dict:
+            return mapping_dict[value]
+        else:
+            return cp.nan  # Return cp.nan for values not found in the mapping_dict
+
     # Create a vectorized version of the mapping function
     value_map = cp.vectorize(map_to_specified_set)
     
     return value_map
+
